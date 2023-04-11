f0b23480fdc441ebc0c3a2f9b4f0d02848b3075a
Ralf Lima <ralfhonorio@gmail.com>
2023-04-11 11:08:19 -0300

    Initial version study with angular

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {ThinkComponent} from "./components/think/think.component";
+import {ListThinkComponent} from "./components/list-think/list-think.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    component: ListThinkComponent,
+    pathMatch: "full"
+  },
+  {
+    path: 'createThink',
+    component: ThinkComponent
+  },
+  {
+    path: 'listThink',
+    component: ListThinkComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { ThinkComponent } from './components/think/think.component';
+import {FormsModule} from "@angular/forms";
+import { ListThinkComponent } from './components/list-think/list-think.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    ThinkComponent,
+    ListThinkComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
